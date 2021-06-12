d1fca553c4ece040b799223511be071c817da022
ryan-lam <ryan-lam@users.noreply.github.com>
2021-06-12 15:59:10 -0400

    1

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.auth',
 	'backend.apps.BackendConfig',
     'corsheaders',
+    'djoser',
 ]
 
 MIDDLEWARE = [
