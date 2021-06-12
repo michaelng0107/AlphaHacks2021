eb7be38c35dd65abbd9a137c73b4f9cca4791f07
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-06-12 18:03:48 -0400

    deleted rest_framework.auth, should be .authtoken

--- project/project/settings.py
+++ project/project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.auth',
     'rest_framework.authtoken',
 	'backend.apps.BackendConfig',
     'corsheaders',
